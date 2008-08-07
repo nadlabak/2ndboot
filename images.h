da8a4f48cdb7b1b97cdfa9095cfe14c49abe1959
Dmitriy Taychenachev <dimichxp@gmail.com>
2008-08-07 17:21:37 +0900

    missing header upload.

---
+++ images.h
@@ -0,0 +1,20 @@
+#ifndef __IMAGES_H__
+#define __IMAGES_H__
+#include "types.h"
+
+#define IMG_LINUX		1
+#define IMG_INITRAMFS		2
+#define IMG_MOTFLATTREE		3
+
+#pragma pack(push,1)
+struct buffer_tag {
+  uint32_t tag;
+  void *data;
+  uint32_t size;
+  uint32_t checksum;
+};
+#pragma pack(pop)
+
+struct buffer_tag *image_find(uint32_t tag);
+
+#endif // __IMAGES_H__
