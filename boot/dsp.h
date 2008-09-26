267a6eb5747c9ea3316df32462d25f61dbec962e
Dmitriy Taychenachev <dimichxp@gmail.com>
2008-09-26 21:40:43 +0900

    Uncoment dsp start prototypes.

--- boot/dsp.h
+++ boot/dsp.h
@@ -8,7 +8,7 @@ int dsp_mem_write_word(addr_t dest, uint32_t word);
 int dsp_mem_read(addr_t src, uint32_t *dest, unsigned int words);
 int dsp_mem_write(addr_t dest, uint32_t *src, unsigned int words);
 
-/* int dsp_start_loader(addr_t paddr); */
-/* int dsp_start_image(addr_t paddr); */
+int dsp_start_loader(addr_t paddr);
+int dsp_start_image(addr_t paddr);
 
 #endif // __HW_DSP_H__
