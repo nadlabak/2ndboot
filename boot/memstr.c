bf2d8a4698c763107e4b70db131b89c6eec3eb82
Dmitriy Taychenachev <dimichxp@gmail.com>
2009-02-14 21:15:46 +0800

    add strcmp func.

--- boot/memstr.c
+++ boot/memstr.c
@@ -47,3 +47,22 @@ char *strcpy(char *d, const char *s) {
   return dest;
 }
 
+int strcmp(const char *s1, const char *s2) {
+  size_t l1, l2;
+  l1 = strlen(s1);
+  l2 = strlen(s2);
+  if (l1 < l2) {
+    return -1;
+  }
+  if (l1 > l2) {
+    return 1;
+  }
+  while (*s1) {
+    if (*s1 != *s2) {
+      return *s1 - *s2;
+    }
+    ++s1;
+    ++s2;
+  }
+  return 0;
+}
--- boot/string.h
+++ boot/string.h
@@ -8,5 +8,6 @@ void *memset(void *d, int c, size_t n);
 int memcmp(const void *s1, const void *s2, size_t n);
 size_t strlen(const char *s);
 char *strcpy(char *d, const char *s);
+int strcmp(const char *s1, const char *s2);
 
 #endif // __STRING_H__
