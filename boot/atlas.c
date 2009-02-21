b19b07c4fe0d0e7ca211cd755b307ee9636908fe
Dmitriy Taychenachev <dimichxp@gmail.com>
2009-02-21 15:39:30 +0800

    add symbolic names for atlas regs (from power_ic).

--- boot/atlas.c
+++ boot/atlas.c
@@ -16,7 +16,7 @@ int atlas_init() {
   return cspi_init(ATLAS_CSPI_MODULE, &cfg);
 }  
 
-int atlas_reg_io(int write, unsigned char reg, uint32_t *p) {
+int atlas_reg_io(int write, atlas_register reg, uint32_t *p) {
   uint32_t outdata, indata;
 
   outdata = (reg & 0x3f) << 0x19;
@@ -39,11 +39,11 @@ int atlas_reg_io(int write, unsigned char reg, uint32_t *p) {
   return 0;
 }
 
-int atlas_reg_read(unsigned char reg, uint32_t *v) {
+int atlas_reg_read(atlas_register reg, uint32_t *v) {
   return atlas_reg_io(0, reg, v);
 }
 
-int atlas_reg_modify(unsigned char reg, uint32_t mask, uint32_t v) {
+int atlas_reg_modify(atlas_register reg, uint32_t mask, uint32_t v) {
   uint32_t foo;
   int ret;
 
@@ -58,11 +58,11 @@ int atlas_reg_modify(unsigned char reg, uint32_t mask, uint32_t v) {
   return atlas_reg_io(1, reg, &foo);
 }
 
-int atlas_reg_write(unsigned char reg, uint32_t v) {
+int atlas_reg_write(atlas_register reg, uint32_t v) {
   return atlas_reg_modify(reg, 0, v);
 }
 
-int atlas_reg_partial(unsigned char reg, uint32_t mask, uint32_t data) {
+int atlas_reg_partial(atlas_register reg, uint32_t mask, uint32_t data) {
   mask &= 0xffffff;
   data &= 0xffffff;
   return atlas_reg_modify(reg, ~mask, data);
